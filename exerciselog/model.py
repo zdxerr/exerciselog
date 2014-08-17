e71dba0f9f1ff0f81d7d0fcadd118c9c7ba5305d
zdxerr <zdxerr@gmail.com>
2014-08-17 19:41:17 +0200

    Added model and parser.

---
+++ exerciselog/model.py
@@ -0,0 +1,101 @@
+
+# from datetime import datetime, date
+import datetime
+from peewee import *
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SqliteDatabase('people.db')
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class User(BaseModel):
+    username = CharField(unique=True)
+    password_hash = CharField()
+    email = CharField(unique=True, null=True)
+    join_date = DateTimeField(default=datetime.datetime.now)
+
+    class Meta:
+        order_by = ('username',)
+
+    @classmethod
+    def create(cls, **kwargs):
+        password = kwargs.pop('password')
+        if password:
+            kwargs['password_hash'] = generate_password_hash(password)
+        return super(User, cls).create(**kwargs)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class Exercise(BaseModel):
+    name = CharField()
+
+
+# Multilanguage Support
+# class ExerciseNames(BaseModel):
+#     exercise = ForeignKeyField(Exercise)
+#     language = CharField()
+#     name = CharField()
+#     description = TextField()
+
+
+class Session(BaseModel):
+    user = ForeignKeyField(User)
+    date = DateField(default=datetime.date.today)
+    description = CharField(null=True)
+    note = TextField(null=True)
+
+
+class Set(BaseModel):
+    session = ForeignKeyField(Session)
+    exercise = ForeignKeyField(Exercise)
+    weight = DecimalField()
+    repetitions = IntegerField()
+
+
+def create_tables():
+    for model in (User, Exercise, Session, Set, ):
+        model.drop_table(fail_silently=True)
+        model.create_table()
+
+
+if __name__ == '__main__':
+    create_tables()
+
+    lutz = User.create(username='Lutz', password='test', email=None)
+
+    session = Session.create(user=lutz, date='2014-08-17', 
+                             note=("Kniebeuge 157,5x1\n"
+                                   "Dücken 62,5x1\n"
+                                   "Kreuzheben 180x1\n"
+                                   "Bankdrücken 95x1\n"))
+
+    sets = [
+        ('Kniebeuge', 157.5, 1, 1),
+        ('Drücken', 62.5, 1, 1),
+        ('Kreuzheben', 180, 1, 1),
+        ('Bankdrücken', 95, 1, 1),
+        ('Kniebeuge', 100, 2, 5),
+    ]
+
+    # print(sets)
+    for exercise, weight, number_of_sets, repetitions in sets:
+        try:
+            exercise = Exercise.get(Exercise.name==exercise)
+        except Exercise.DoesNotExist:
+            exercise = Exercise.create(name=exercise)
+        for set_number in range(1, number_of_sets + 1):
+            Set.create(session=session, 
+                       exercise=exercise, 
+                       weight=weight, 
+                       repetitions=repetitions)
+
+
+    for s in Set.select().where(Set.session==session):
+        print(s.exercise.name, s.weight, s.repetitions)
+
+        
---
+++ exerciselog/parser.py
@@ -0,0 +1,60 @@
+
+import decimal
+from pyparsing import *
+
+ws = ' \t'
+ParserElement.setDefaultWhitespaceChars(ws)
+
+# Define punctuation and legal characters.
+backslash = '\\'
+hashmark = '#'
+colon = ':'
+
+unicode_printables = ''.join(chr(c) for c in range(65536)
+                             if not chr(c).isspace())
+
+standard_chars = unicode_printables.replace(backslash, '').replace(hashmark, '')
+
+
+float_number = Regex(r'\d+(\,\d*)?').setParseAction( lambda s, l, t: [ decimal.Decimal(t[0].replace(',', '.', 1)) ] )
+
+unit = oneOf(('kg', 'lbs')).setResultsName('unit')
+
+weight = float_number.setResultsName('weight') + Optional(unit)
+
+# ws = ' \t'
+exercise = Word(unicode_printables).setResultsName('exercise')
+
+integer = Word(nums).setParseAction( lambda s,l,t: int(t[0]) )
+
+times = Suppress('x') + integer
+
+# repetitions = Suppress('x') + integer
+
+sets_reps = times.setResultsName('x1') + Optional(times).setResultsName('x2')
+
+reps = (Suppress('x') + integer + OneOrMore(Suppress('|') + integer)).setResultsName('reps')
+
+exercise_set = exercise + OneOrMore(weight + Or((sets_reps, reps))).setResultsName('weight_reps')
+
+tests = [
+    ('Squat 125x3x5', ['Squat', decimal.Decimal(125), 3, 5]),
+    ('Drücken 60x3x5', ['Drücken', 60.0, 3, 5]),
+    ('Drücken 62,5x1x5', ['Drücken', 62.5, 1, 5]),
+    ('Drücken 62,5kg x 1 x 5', ['Drücken', 62.5, 'kg', 1, 5]),
+    ('Bankdrücken 97,5x3', ['Bankdrücken', 97.5, 3]),
+    ('Bankdrücken 220lbsx3 ', ['Bankdrücken', 220, 'lbs', 3]),
+    ('Kreuzheben 400kgx3|2|1 ', ['Bankdrücken', 220, 'lbs', 3]),
+    ('Kreuzheben 300x5|4 250x4x4 200x3|2|1 ', ['Bankdrücken', 220, 'lbs', 3]),
+    # ('Drücken 62,5kg x 1 x 5', ['Drücken', 62.5, 'kg', 3, 5]),
+]
+
+
+for test, expected in tests:
+    result = exercise_set.parseString(test)
+    print("Parsing `{}` resulted in {} (expected: {}) -> {}".format(
+          test, result, expected, 
+          'PASSED' if list(result) == expected else 'FAILED'))
+
+
+
